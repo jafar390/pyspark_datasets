893f1d8a00d9d24977f35ce238ff380876fdcce5
Mayraju <53441673+Mayraju@users.noreply.github.com>
2021-04-23 23:23:54 +0530

    Update model.py

--- LR/model.py
+++ LR/model.py
@@ -59,7 +59,7 @@ df_transformed.show()
 
 logger.error("#### before spliting")
 
-train,test = df_transformed.randomSplit([float64(sys.argv[1]), float(sys.argv[2])],seed=7)
+train,test = df_transformed.randomSplit([float(sys.argv[1]), float(sys.argv[2])],seed=7)
 #train60,test40 = df_transformed.randomSplit([0.6,0.4],seed=7)
 #train70,test30 = df_transformed.randomSplit([0.7, 0.3], seed=7)
 #train80,test20 = df_transformed.randomSplit([0.8,0.2],seed=7)
