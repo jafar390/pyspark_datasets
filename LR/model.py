e502f8441926e4f7f680c1db802d595a80fcddca
Mayraju <53441673+Mayraju@users.noreply.github.com>
2021-04-23 22:36:44 +0530

    Create model.py

---
+++ LR/model.py
@@ -0,0 +1,92 @@
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import OneHotEncoder
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler,StringIndexer,VectorIndexer,MinMaxScaler
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.tuning import ParamGridBuilder,CrossValidator
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.sql import SparkSession
+import sys
+
+
+
+dataset = "/opt/dkube/dataset"
+spark = SparkSession.builder.config("spark.driver.memory","15g").appName('imbalanced_multi_data').getOrCreate()
+Logger= spark._jvm.org.apache.log4j.Logger
+
+
+if(len(sys.argv)< 2):
+  logger.error("arguements are requires : note: run like this:python3 <filename.py> 0.6 0.4")
+  sys.exit(1)
+  
+
+logger.error("############ this is test message")
+
+new_df = spark.read.csv(dataset + '/UNSW-Nb4.csv',header=True,inferSchema=True)
+
+
+#Convert qualification and gender columns to numeric
+col2_indexer = StringIndexer(inputCol="col2", outputCol="col2Index")
+col4_indexer = StringIndexer(inputCol="col4",outputCol="col4Index")
+col5_indexer = StringIndexer(inputCol="col5", outputCol="col5Index")
+col6_indexer = StringIndexer(inputCol="col6", outputCol="col6Index")
+
+#Convert qualificationIndex and genderIndex
+logger.error("############ initiated onehot encoder algo")
+onehot_encoder = OneHotEncoder(inputCols=["col2Index","col4Index","col5Index","col6Index"], outputCols=["col2_vec", "col4_vec","col5_vec","col6_vec"])
+logger.error("############ onehot encoder done")
+#Merge multiple columns into a vector column
+logger.error("############ intiated vector assembler")
+vector_assembler = VectorAssembler(inputCols=['col7','col8','col9','col10','col11','col12','col13','col15','col16','col17','col18','col19','col20','col21','col22','col23','col24','col25','col26','col27','col28','col29','col30','col31','col32','col33','col34','col35','col36','col37','col38','col39','col41','col42','col43','col44','col45','col46','col47',"col2_vec", "col4_vec","col5_vec","col6_vec"], outputCol='features')
+logger.error("############ vector assembler done")
+#Create pipeline and pass it to stages
+pipeline = Pipeline(stages=[
+           col2_indexer,col4_indexer, 
+           col5_indexer,col6_indexer,
+           onehot_encoder,
+           vector_assembler
+])
+#fit and transform
+df_transformed = pipeline.fit(new_df).transform(new_df)
+df_transformed.show()
+
+
+train,test = df_transformed.randomSplit([argv[0], argv[1],seed=7)
+#train60,test40 = df_transformed.randomSplit([0.6,0.4],seed=7)
+#train70,test30 = df_transformed.randomSplit([0.7, 0.3], seed=7)
+#train80,test20 = df_transformed.randomSplit([0.8,0.2],seed=7)
+#train90,test10 = df_transformed.randomSplit([0.9,0.1],seed=7)
+
+
+
+minmax = MinMaxScaler(inputCol="features",outputCol="normFeatures")
+lr = LogisticRegression(labelCol="label",featuresCol="normFeatures",maxIter=10,regParam=0.3)
+
+stages1 = []
+stages1 +=[minmax]
+stages1 += [lr]
+
+logger.error("logistic regression","train",argv[0],"test",argv[1])
+
+from pyspark.ml import Pipeline
+
+pipeline = Pipeline().setStages(stages1)
+#model = pipeline.fit(train)
+lr_model60 = pipeline.fit(train)
+lr_pp_df60 = lr_model60.transform(test)
+
+lr_predicited40 = lr_pp_df60.select("normFeatures","prediction","label","rawPrediction","probability")
+tp = float(lr_predicited40.filter("prediction == 1.0 AND label == 1").count())
+fp = float(lr_predicited40.filter("prediction == 1.0 AND label == 0").count())
+tn = float(lr_predicited40.filter("prediction == 0.0 AND label == 0").count())
+fn = float(lr_predicited40.filter("prediction == 0.0 AND label == 1").count())
+
+acc = float((tp+tn)/lr_predicited40.count())
+pr = tp / (tp + fp)
+
+re = tp / (tp + fn)
+
+metrics40 = spark.createDataFrame([("TP",tp),("FP",fp),("TN",tn),("FN",fn),("accuracy",acc),("precision",pr),("Recall",re),("F1",2*pr*re/(re+pr))],["metric","value"])
+metrics40.show()
